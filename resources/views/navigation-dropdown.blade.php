71457966fdea3a7a613911c395d03de1f9303b16
Ceansbzh <ceansbzh@gmail.com>
2020-11-28 15:26:01 +0100

    Add bank route to nav

--- resources/views/navigation-dropdown.blade.php
+++ resources/views/navigation-dropdown.blade.php
@@ -16,6 +16,11 @@
                         {{ __('Dashboard') }}
                     </x-jet-nav-link>
                 </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-jet-nav-link href="{{ route('bank.dashboard') }}" :active="request()->routeIs('bank.dashboard')">
+                        {{ __('Bank') }}
+                    </x-jet-nav-link>
+                </div>
             </div>
 
             <!-- Settings Dropdown -->
@@ -119,6 +124,11 @@
         <div class="pt-2 pb-3 space-y-1">
             <x-jet-responsive-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
+            </x-jet-responsive-nav-link>
+        </div>
+        <div class="pt-2 pb-3 space-y-1">
+            <x-jet-responsive-nav-link href="{{ route('bank.dashboard') }}" :active="request()->routeIs('bank.dashboard')">
+                {{ __('Bank') }}
             </x-jet-responsive-nav-link>
         </div>
 
